78d75e5769ed9157cda055ab1f5014b8110c14eb
pp35 <poornimapandey3590.com>
2024-10-07 15:56:31 +0530

    first

--- program1.js
+++ program1.js
@@ -3,8 +3,39 @@
  * @return {boolean}
  */
 var isValid = function(s) {
+   
+    let stack = [];
     
+    
+    const bracketMap = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    };
+
+   
+    for (let i = 0; i < s.length; i++) {
+        let char = s[i];
+        
+        
+        if (bracketMap[char]) {
+            
+            let topElement = stack.length > 0 ? stack.pop() : '#';
+            
+          
+            if (topElement !== bracketMap[char]) {
+                return false;
+            }
+        } else {
+            
+            stack.push(char);
+        }
+    }
+
+   
+    return stack.length === 0;
 };
+
 
 module.exports = { isValid };
 
